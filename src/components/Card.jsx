bb46cbff60b02232a335e18112b68e7e941141dc
creativoma <ma.marianoalvarez@gmail.com>
2022-04-20 00:20:09 +0200

    Fixed image path

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,10 +3,10 @@ import '../index.css'
 export default function Card(props) {
     return (
         <div className="card">
-            <img className='img-card' src={`../../public/img/${props.imageUrl}`} alt={props.title} />
+            <img className='img-card' src={`../../img/${props.imageUrl}`} alt={props.title} />
             <div className='container-info'>
                 <div className='container-location'>
-                    <img src='../../public/img/icon-location.svg' />
+                    <img src='../../img/icon-location.svg' />
                     <p> {props.location}</p>
                     <a href={props.googleMapsUrl} target="_blank">View on Google Maps</a>
                 </div>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,7 +5,7 @@ export default function NavBar() {
     return (
         <header>
             <div className='header-container'>
-               <img className='header-logo' src="../../public/img/logo-world.svg" alt="Travel Journal" />
+               <img className='header-logo' src="../../img/logo-world.svg" alt="Travel Journal" />
                <a href='/'>my travel journal.</a>
             </div>
         </header>
